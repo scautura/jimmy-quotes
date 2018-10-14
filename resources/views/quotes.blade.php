d7a82417e1c4d45f6c2c43e45a36adcfd5e277d3
Scautura <github@scautura.co.uk>
2018-10-14 23:34:36 +0100

    Added Bootstrap to make things cleaner

--- resources/views/quotes.blade.php
+++ resources/views/quotes.blade.php
@@ -1,25 +1,31 @@
 <html>
     <head>
-        <title>@yield('title')</title>
+        <title>Jimmy Broadbent's Discord Quotes</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
-        <table>
-            <tr>
-                <td>Quote number</td>
-                <td>Added By</td>
-                <td>Author ID</td>
-                <td>Author Name</td>
-                <td>Quote</td>
-            </tr>
-            @foreach ($quotes as $quote)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $quote["added_by"] }}</td>
-                <td>{{ $quote["author_id"] }}</td>
-                <td>{{ $quote["author_name"] }}</td>
-                <td>{{ $quote["text"] }}</td>
-            </tr>
+        <table class="table">
+            <thead>
+                <tr>
+                    <td>Quote number</td>
+                    <td>Added By</td>
+                    <td>Author ID</td>
+                    <td>Author Name</td>
+                    <td>Quote</td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($quotes as $quote)<tr>
+                    <th scope="row">{{ $loop->iteration }}</td>
+                    <td>{{ $quote["added_by"] }}</td>
+                    <td>{{ $quote["author_id"] }}</td>
+                    <td>{{ $quote["author_name"] }}</td>
+                    <td>{{ $quote["text"] }}</td>
+                </tr>
             @endforeach
+            </tbody>
         </table>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="{{ asset('js/app.min.js') }}"></script>
     </body>
 </html>
